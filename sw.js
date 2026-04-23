6cfe094466f16bc05cd851f7c65f8ef09b364ed8
Qurrat <qurratulain.06@gmail.com>
2026-04-23 14:51:25 -0400

    Add update banner for new version notifications and improve service worker caching

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'vibey-cache-v1';
+const CACHE_NAME = 'vibey-cache-v2';
 const ASSETS = [
   '/Vibey/index.html',
   '/Vibey/manifest.json'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
@@ -14,13 +15,16 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames
-          .filter((name) => name !== CACHE_NAME)
-          .map((name) => caches.delete(name))
-      );
-    })
+    Promise.all([
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames
+            .filter((name) => name !== CACHE_NAME)
+            .map((name) => caches.delete(name))
+        );
+      }),
+      clients.claim()
+    ])
   );
 });
 
